732b4f32e3abf2a265e29346b70fed76491f8d1e
Zarema <zarema.zaynasheva@gmail.com>
2023-12-23 11:32:34 +0300

    python task

---
+++ square_every_digit.py
@@ -0,0 +1,19 @@
+def square_digits(num):
+  num = str(num)
+  #int_list = [int(n) for n in num]
+  #square_list = [n** 2 for n in int_list]
+  square_list = [(int(n)** 2) for n in num]
+  print(square_list)
+  result = int(''.join([str(n) for n in square_list]))
+  print(result)
+
+square_digits(765)
+
+# Solution
+def square_digits(num):
+  num = str(num)
+  #int_list = [int(n) for n in num]
+  #square_list = [n** 2 for n in int_list]
+  square_list = [(int(n)** 2) for n in num]
+  result = int(''.join([str(n) for n in square_list]))
+  return result
