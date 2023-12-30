018eff87ec9a8d5d56aa1e6976bbf733c7dd927f
Starburned7 <starburned7@yandex.ru>
2023-12-30 16:14:57 +0300

    daily python task

---
+++ find_added.py
@@ -0,0 +1,11 @@
+def find_added(st1, st2):
+    arr1 = list(st1)
+    arr2 = list(st2)
+    for i in arr1:
+        if i in arr2:
+            arr2.remove(i)
+    result = ''.join(sorted(arr2))
+    print(result)
+    return result
+
+find_added('4455446', '447555446666')
